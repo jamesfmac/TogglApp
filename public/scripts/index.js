158853fe2c9b64ce94a262d9d07b8cbab4202196
James McManamey <jfmcmanamey@gmail.com>
2016-02-17 08:30:36 +1100

    form now submits data via post

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,22 @@
 var test = function () {
     var inputstartdate =
-        $('#startdate').val()
+        $('#startdate').val();
     var inputenddate =
-        $('#endate').val()
+        $('#endate').val();
+    var data = {
+        "user": "me!"
 
+    };
+    /*
+                  $.ajax({
+                      type: "POST",
+                      url: "/api/timelogs",
+                      processData: false,
+                      contentType: 'application/json',
+                      data: JSON.stringify(data)
+                  });
+                  console.log(data);
+                  */
 };
 
 
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require("express");
+var bodyParser = require('body-parser')
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
-
+var jsonParser = bodyParser.json();
 
 app.use(express.static('public'));
+app.use(bodyParser());
 
 router.use(function (req, res, next) {
     console.log("/" + req.method);
@@ -14,6 +16,21 @@ router.use(function (req, res, next) {
 
 router.get("/", function (req, res) {
     res.sendFile(path + "index.html");
+});
+
+app.post('/', function (request, response) {
+    console.log(request.body.startdate);
+    console.log(request.body.enddate);
+    response.end;
+});
+
+router.post("/api/timelogs", function (req, res) {
+    var recieed = req.body
+    var string = JSON.parse(recieed);
+    var sdate = req.params.user
+    var edate = req.params.edate
+    res.send('start date is ' + sdate);
+    console.log(req.url + recieed + string)
 });
 
 
