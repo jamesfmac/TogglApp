a705365488515e31a8b1a98977c816a7856ce013
James McManamey <jfmcmanamey@gmail.com>
2016-02-16 23:11:40 +1100

    added input form for start and end date

---
+++ public/scripts/index.js
@@ -0,0 +1,14 @@
+var test = function () {
+    var inputstartdate =
+        $('#startdate').val()
+    var inputenddate =
+        $('#endate').val()
+
+};
+
+
+
+
+
+
+document.getElementById("btnrequestextract").addEventListener("click", test)
